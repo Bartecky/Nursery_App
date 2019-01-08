e7e98b492fd1a813d4c48da2a75651de48c9c859
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-01-09 00:09:30 +0100

    created Activity and Diet Model, linked with Child model by M2M relations

--- src/NurseryApp/models.py
+++ src/NurseryApp/models.py
@@ -22,6 +22,8 @@ class Child(models.Model):
     last_name = models.CharField(max_length=64)
     day_of_birth = models.DateField()
     group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.CASCADE)
+    activity = models.ManyToManyField('Activity')
+    diet = models.ManyToManyField('Diet')
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
@@ -68,4 +70,14 @@ class Caregiver(models.Model):
         return reverse_lazy('caregiver-detail-view', kwargs={'pk': self.pk})
 
 
+class Activity(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Diet(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True, null=True)
