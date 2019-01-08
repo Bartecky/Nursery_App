80bd58b7d1fd204cb70c847bf47c3b2b0650be92
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-01-08 21:48:35 +0100

    added Group model (relation with Child)

--- src/NurseryApp/models.py
+++ src/NurseryApp/models.py
@@ -19,9 +19,10 @@ class Child(models.Model):
     parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+    group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.CASCADE)
+    day_of_birth = models.DateField()
 
-    # date_of_birth
-    # group
+
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
@@ -29,6 +30,20 @@ class Child(models.Model):
     class Meta:
         verbose_name_plural = 'children'
         ordering = ['last_name']
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+
+
+
+
+# Group
+# Teacher
+# Waiting list?
+# Activity
+# Caregiver
 
 
 
